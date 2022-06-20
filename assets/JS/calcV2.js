32a08bf4e102a7265c67d888f527fa2e64c52f06
GamerBike39 <champidub@gmail.com>
2022-06-20 19:28:23 +0200

    plus d'anim

--- assets/JS/calcV2.js
+++ assets/JS/calcV2.js
@@ -1,13 +1,17 @@
 const output = document.querySelector(".output");
-const result = document.querySelector(".ecran");
+let result = document.querySelector(".ecran");
 const keys = document.querySelectorAll("button");
 const toucheReload = document.querySelector(".ac");
 const ici = document.querySelector(".ici");
 const calculatrice = document.querySelector(".calculatrice");
+const cheat = document.querySelector(".cheatcode");
+const cheatS = document.querySelector(".cheatcodeS");
+const soleil = document.querySelector(".cheatcodeS p:nth-last-child(1)");
+const cochon = document.querySelector(".cheatcode p:nth-last-child(1)");
 
 keys.forEach((key) => {
   key.addEventListener("click", calculer);
-  console.log(key);
+  // console.log(key);
 });
 
 function calculer() {
@@ -16,6 +20,7 @@ function calculer() {
   if (buttonText === "AC") {
     output.innerText = "";
     result.innerText = "";
+    // définir le fichier son
     let reload = document.querySelector(".sound");
     reload.volume = 1;
     toucheReload.addEventListener("click", () => {
@@ -44,6 +49,45 @@ function calculer() {
     output.style.animation = "small 0.2s ease-in-out";
     result.style.animationFillMode = "forwards";
     output.style.animationFillMode = "forwards";
+  }
+  if (output.textContent === "35383773") {
+    calculatrice.classList.add("retourne");
+    cheat.classList.add("changeColor");
+    cochon.classList.add("grossir");
+
+    setTimeout(() => {
+      calculatrice.classList.add("retourneBonsens");
+      cheat.classList.remove("changeColor");
+      cochon.classList.remove("grossir");
+    }, 4000);
+
+    setTimeout(() => {
+      calculatrice.classList.remove("retourne");
+      calculatrice.classList.remove("retourneBonsens");
+      output.innerHTML = "";
+      result.innerHTML = "";
+    }, 5000);
+    return;
+  }
+
+  if (output.textContent === "713705") {
+    calculatrice.classList.add("retourne");
+    cheatS.classList.add("changeColor");
+    soleil.classList.add("grossir");
+
+    setTimeout(() => {
+      calculatrice.classList.add("retourneBonsens");
+      cheatS.classList.remove("changeColor");
+      soleil.classList.remove("grossir");
+    }, 4000);
+
+    setTimeout(() => {
+      calculatrice.classList.remove("retourne");
+      calculatrice.classList.remove("retourneBonsens");
+      output.innerHTML = "";
+      result.innerHTML = "";
+    }, 5000);
+    return;
   } else {
     output.textContent += buttonText;
     return;
