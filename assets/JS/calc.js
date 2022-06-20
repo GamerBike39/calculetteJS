e095f458f1bcb49f683f163e3fdd992d4c2a23d8
GamerBike39 <champidub@gmail.com>
2022-06-20 09:15:05 +0200

    update animation

--- assets/JS/calc.js
+++ assets/JS/calc.js
@@ -1,67 +1,86 @@
-// DOM
-// on récupère toute les touches avec le dom et le queryselectorALL la classe .bouton
-const touches = [...document.querySelectorAll('.bouton')];
-const ecran = document.querySelector('.ecran');
-// console.log(touches); 
-// renvoi une node list, ce n'est pas un tableau. on le transforme en tableau (array) avec l'utilisation de  [... ]
-// une node list ne permet pas forcément les actions que l'ont peut faire avec un tableau 
+const touches = [...document.querySelectorAll(".bouton")];
+const ecran = document.querySelector(".ecran");
+const blague = document.querySelector(".egal");
+const pasVoir = document.querySelector(".pasVoir");
+// renvoi une node list, ce n'est pas un tableau. on le transforme en tableau avec l'utilisation de  [... ]
 
-// la fonction map() prend chaque élément d'un tableau, fait quelque chose à cet élément (fonction callbak) et retourne le résulat sous forme d'un nouveau tableau (ici dans la variable nouveauTab)
+// la fonction map() prend chaque élément d'un tableau, fait quelque chose à cet élément (fonction callbak : une fonction passée dans une autre fonction en tant qu'argument, qui est ensuite invoquée à l'intérieur de la fonction externe pour accomplir une sorte de routine ou d'action. L' exemple ci-dessus est un rappel synchrone et il est exécuté immédiatement) et retourne le résulat sous forme d'un nouveau tableau (ici dans la variable nouveauTab)
 // const nouveauTab = taleau.map( element => faireQqch(element));
-const listeKeycode = touches.map(touche => touche.dataset.key);
-// console.log(listeKeycode);
+const listeKeycode = touches.map((touche) => touche.dataset.key);
+console.log(listeKeycode);
 // affiche l'ensemble des touches avec leur keycode à l'intérieur de la classe .touches // ces keycodes sont sous la forme de phrases c'est important pour la suite.
 
-document.addEventListener('keydown', (e)=> {
-    const valeur = e.keyCode.toString();
-    calculer(valeur);
-    // console.log(valeur, typeof valeur);
+document.addEventListener("keydown", (e) => {
+  const valeur = e.keyCode.toString();
+  calculer(valeur);
 });
 
-document.addEventListener('click', (e) => {
-    const valeur = e.target.dataset.key;
-    calculer(valeur);
-    // console.log(valeur, typeof valeur)
-})
-
-// permet d'"écouter" l'évènement souhaité que ce soit une pression de touche comme avec keydown, ou un clic avec onclick
+document.addEventListener("click", (e) => {
+  const valeur = e.target.dataset.key;
+  calculer(valeur);
+  // console.log(valeur, typeof valeur)
+});
 
 //fonction calculer
 const calculer = (valeur) => {
-    if(listeKeycode.includes(valeur)) {
-        switch(valeur){
-            case '8' :
-                ecran.textContent = "";
-                break;
-            case '13':
-                const calcul = eval(ecran.textContent);
-                ecran.textContent = calcul;
-                break;
-            case '46':
-                const del = ecran.textContent.slice(0, -1);
-                ecran.textContent = del;
-                break;
-            default : 
-            const indexKeyCode = listeKeycode.indexOf(valeur);
-            const touche = touches[indexKeyCode];
-            ecran.textContent += touche.innerHTML;
-        }
+  if (listeKeycode.includes(valeur)) {
+    switch (valeur) {
+      case "8":
+        ecran.innerHTML = "";
+        break;
+      case "13":
+        const calcul = eval(ecran.textContent);
+        ecran.textContent = calcul;
+        break;
+      case "46":
+        const del = ecran.textContent.slice(0, -1);
+        ecran.textContent = del;
+        break;
+      default:
+        const indexKeyCode = listeKeycode.indexOf(valeur);
+        const touche = touches[indexKeyCode];
+        ecran.textContent += touche.innerHTML;
     }
+  }
+};
 
-}
-  
-window.addEventListener('error', (e) => {
-
-	ecran.textContent = ('⛔ ERREUR ⛔');
-})
+window.addEventListener("error", (e) => {
+  ecran.textContent = "ERREUR";
+});
 
 // animation souris
 const mousemove = document.querySelector(".mouse");
 
 window.addEventListener("mousemove", (e) => {
-   mousemove.style.left = e.pageX + "px";
-});
-window.addEventListener("mousemove", (e) => {
-   mousemove.style.top = e.pageY + "px";
+  mousemove.style.left = e.pageX + "px";
+  mousemove.style.top = e.pageY + "px";
 });
 
+// blague
+blague.addEventListener("dblclick", () => {
+  blague.style.top = Math.floor(Math.random() * 95) + "vh";
+  blague.style.left = Math.floor(Math.random() * 95) + "vw";
+  blague.classList.toggle("blague");
+  pasVoir.classList.toggle("pasVoir");
+});
+
+blague.addEventListener("click", () => {
+  blague.classList.toggle("blagueNon");
+});
+
+// effets sonore sur les touches
+var reload = document.querySelector(".sound");
+reload.volume = 0.5;
+
+toucheReload = document.querySelector(".ac");
+toucheReload.addEventListener("click", () => {
+  reload.play();
+  toucheReload.classList.add("gaucheDroite");
+  ecran.classList.add("ecranFeu");
+  setTimeout(() => {
+    toucheReload.classList.remove("gaucheDroite");
+    ecran.classList.remove("ecranFeu");
+  }, 700);
+});
+
+console.log(ecran);
